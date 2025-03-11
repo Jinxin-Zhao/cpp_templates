dda38233fe26f80050bf5497a4c5c4704f3c585c
jasper <zhaojinxinfdu@gmail.com>
2025-03-11 22:51:42 +0800

    learn tpp param & TypeFunctions

--- chapter/chapter_19.h
+++ chapter/chapter_19.h
@@ -1,68 +1,192 @@
 #ifndef _CHAPTER_19_H_
 #define _CHAPTER_19_H_
+#include <vector>
+#include <list>
 
 // type traits
 template <typename T>
 struct AccumulationTraits;
 
-// c++中只允许对在类中对一个整型和枚举类型的static const进行初始化
+// c++中只允许在类中对一个整型和枚举类型的static const进行初始化
 template<>
 struct AccumulationTraits<char>{
     using AccT = int;
     // value traits
-    static AccT const zero = 0;
+    // static AccT const zero = 0;
+    // rewrite with inline member functions for value traits, such a function can be decalred 'constexpr' if it returns a literal type
+    static constexpr AccT zero() { return 0; }
 };
 
 template<>
 struct AccumulationTraits<short> {
     using AccT = int;
-    static AccT const zero = 0;
+    // static AccT const zero = 0;
+    static constexpr AccT zero() { return 0; }
 };
 
 template<>
 struct AccumulationTraits<int> {
     using AccT = int;
-    static AccT const zero = 0;
+    // static AccT const zero = 0;
+    static constexpr AccT zero() { return 0; }
 };
 
 template<>
 struct AccumulationTraits<unsigned int> {
     using AccT = unsigned long;
-    static AccT const zero = 0;
+    // static AccT const zero = 0;
+    static constexpr AccT zero() { return 0; }
 };
 
 template<>
 struct AccumulationTraits<float> {
     using AccT = float;
-    static constexpr float zero = 0.0f;
+    // static constexpr float zero = 0.0f;
+    static constexpr AccT zero() { return 0; }
 };
 
-// 对于自定义的非字面值如
+// a user-defined arbitrary-precision 'BigInt' type might not be a literal type
+// typically it has to allocate components on the heap
 class BigInt{
 public:
-    BigInt(long long){}
+    BigInt(long long){
+        // allocate some space on heap ...
+    }
 };
 
 template<>
 struct AccumulationTraits<BigInt> {
     using AccT = BigInt;
     // error
-    //static constexpr BigInt zero = BigInt{0};
+    //static constexpr BigInt zero = BigInt{0}; // Error: not a literal type
+
     // solution: add inline keywords
-    inline static BigInt const zero = BigInt{0};
+    // inline static BigInt const zero = BigInt{0}; // OK since C++17
+
+    static BigInt zero() { return BigInt{0}; }
 };
 
-template<typename T, typename AT = AccumulationTraits<T>>
-auto accum(T * begin, T * end) {
-    // using AccT = typename AccumulationTraits<T>::AccT;
-    // AccT total =  AccumulationTraits<T>::zero;
-    typename AT::AccT total = AT::zero;
+// template<typename T, typename AT = AccumulationTraits<T>>
+// auto accum(T * begin, T * end) {
+//     // using AccT = typename AccumulationTraits<T>::AccT;
+//     // AccT total =  AccumulationTraits<T>::zero;
+//     typename AT::AccT total = AT::zero();
+//     while (begin != end) {
+//         total += *begin;
+//         ++begin;
+//     }
+//     return total;
+// }
+
+
+/** Traits versus Policies and Policy classes */
+
+class SumPolicy {
+public:
+    template <typename T1, typename T2>
+    static void accumulate(T1 & total, T2 const & value) {
+        total += value;
+    }
+};
+
+// by specifying a different policy to accumulate values, we can compute different things.
+class MultPolicy {
+public:
+    template <typename T1, typename T2>
+    static void accumulate(T1 & total, T2 const & value) {
+        total *= value;
+    }
+};
+
+
+template <typename T1, typename T2>
+class SumPolicyTpp {
+public:
+    static void accumulate(T1 & total, T2 const & value) {
+        total += value;
+    }
+};
+
+
+// c++ standard lib takes initial value as a third (function call) arg
+template<typename T, 
+    typename Policy = SumPolicy,
+    typename Traits = AccumulationTraits<T>>
+auto accum(T const * begin, T const * end) {
+    using AccT = typename Traits::AccT;
+    AccT total = Traits::zero();
     while (begin != end) {
-        total += *begin;
+        Policy::accumulate(total, *begin);
         ++begin;
     }
     return total;
 }
 
+// 19.2.2 member templates versus template template parameters 
+// advantage of accessing policy classes through template template
+// parameters is that it makes it easier to have a policy class carry with it some state information (i.e., static data members)
+template<typename T, 
+    template<typename, typename> class Policy = SumPolicyTpp,
+    typename Traits = AccumulationTraits<T>>
+auto accum_tpp(T const * begin, T const * end) {
+    using AccT = typename Traits::AccT;
+    AccT total = Traits::zero();
+    while (begin != end) {
+        Policy<AccT, T>::accumulate(total, *begin);
+        ++begin;
+    }
+    return total;
+}
+
+// 19.2.4 accumulation with general iterators
+#include <iterator>
+
+template <typename Iter>
+auto accumIter(Iter start, Iter end) {
+    using VT = typename std::iterator_traits<Iter>::value_type;
+    VT total{};
+    while (start != end) {
+        total += * start;
+        ++start;
+    }
+    return total;
+}
+
+// 19.3 type functions
+template <typename T>
+struct TypeSize {
+    static std::size_t const value = sizeof(T);
+};
+// use: TypeSize<int>::value
+
+template <typename T>
+struct ElementT; // primary template
+
+template <typename T>
+struct ElementT<std::vector<T>> {  // partial specialization for vector
+    using Type = T;
+};
+
+template <typename T>
+struct ElementT<std::list<T>> {
+    using Type = T;
+};
+
+
+template <typename T, std::size_t N>
+struct ElementT<T[N]> {  // partial specialization for vector
+    using Type = T;
+};
+
+template <typename T>
+struct ElementT<T[]> {
+    using Type = T;
+};
+
+// use
+template <typename T>
+void printElemType (T const & c) {
+    std::cout << "Container of " << typeid(typename ElementT<T>::Type).name() << " elements. \n";
+}
 
 #endif
--- unit_test/ut_chapter_19/ut_chapter_19.cpp
+++ unit_test/ut_chapter_19/ut_chapter_19.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
+#include <chrono>
 #include "../gtest/include/gtest/gtest.h"
 #include "../../chapter/chapter_19.h"
 
+
+
+
 TEST(TestChapterEight, test_traits) {
-    char name[] = "templates";
-    int length = sizeof(name) - 1;
-    std::cout << "accum average tempaltes " << accum(name, name + length) / length << std::endl;
+    // char name[] = "templates";
+    // int length = sizeof(name) - 1;
+    // std::cout << "accum average tempaltes " << accum(name, name + length) / length << std::endl;
+    //
+    static int num[] = {1, 2, 3, 4, 5};
+    std::cout << "the result is " << accum<int, MultPolicy>(num, num + 5) << std::endl;
+
+    std::vector<bool> s;
+    printElemType(s);
+    int arr[43];
+    printElemType(arr);
 }
 
 int main (int argc, char** argv) {
     testing::InitGoogleTest(&argc,argv);
     return RUN_ALL_TESTS();
+
+    return 0;
 }
